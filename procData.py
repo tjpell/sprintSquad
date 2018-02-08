5f640a9a1b1b5b36d083fb91c76fb188fd4fb840
tpellerin51 <tpellerin51@gmail.com>
2018-02-08 15:15:49 -0800

    fix this

--- procData.py
+++ procData.py
@@ -6,12 +6,12 @@ def extract_Data(infile, outfile):
     with open(infile) as f:
         lines = f.readlines()
     lines = [json.loads(l) for l in lines]
-    lines = [l for l in lines if l.get('prop').get('age') and l.get('name')]
+    lines = [l for l in lines if 'name' in l and 'prop' in l and 'age' in l['prop']] #keys exist
+    lines = [l for l in lines if l.get('name') and l.get('prop').get('age')] #keys aren't null
     names = [l['name'] for l in lines]
     ages = [l['prop']['age'] for l in lines]
     with open(outfile, 'w') as f:
-        for n, a in zip(names, ages):
-            print n + '\t' + str(a) + '\n'
+        for n,a in zip(names, ages):
             f.write(n + '\t' + str(a) + '\n')
 
 os.system('cd ../..')
@@ -22,12 +22,13 @@ path = '/srv/runme/'
 
 os.system('rm ' + path + prefix + '.txt')
 
-# files = glob.glob(path + 'prefix*')
+files = glob.glob(path + prefix + '*')
 
-files = [filename for filename in os.listdir('.') if filename.startswith("prefix")]
+# files = [filename for filename in os.listdir('.') if filename.startswith(prefix)]
 print "files:" , files
+
 for f in files:
     try:
         extract_Data(f, path + '{}.txt'.format(prefix))
-    except ValueError:
+    except ValueError: #catches non-json files
         pass
