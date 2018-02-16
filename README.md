70512fd84bb70823f661fe3e7ac2ae452ecff479
tpellerin51 <tpellerin51@gmail.com>
2018-02-16 10:40:46 -0800

    act on code review comments

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sprintSquad
-A home for all code for the sprint project!
+
+All of the code required to launch a JSON ingestion engine.
+
+Files:
+deploy.py :
+1) will connect to ec2 instance, and pull up git repo
+2) write procData to cron file to be executed every 5 minutes. passes along a file name prefix
+
+procData.py :
+1) for every file that starts with prefix, read it
+2) if line in file is porperly formed JSON blob, extract name and age and write to output file
--- procData.py
+++ procData.py
@@ -3,56 +3,52 @@ import json
 import os
 import sys
 
-def extract_Data(infile, output):
+def extract_Data(infile, outlist):
     """
-    Input: file 
-    Output: list of strings containing names and ages 
-    Function extracts JSON blobs from lines of input file
+    Infile: file of JSON blobs to be processed
+    Output: list of strings containing names and ages extracted from properly formatted JSON blobs
     """
-    lines = []
+
+    json_lines = []
     with open(infile) as f:
-        oldlines = f.readlines()
-    if oldlines:
-        for i in range(len(oldlines)):
+        input_lines = f.readlines()
+    if input_lines:
+        for i in range(len(input_lines)):
             try:
-                lines.append(json.loads(oldlines[i]))
-            except ValueError: #catches non-json format
-                pass
-        lines = [l for l in lines if 'name' in l and 'prop' in l and 'age' in l['prop']] #keys exist
-        lines = [l for l in lines if l.get('name') and l.get('prop').get('age')] #keys aren't null
+                json_lines.append(json.loads(input_lines[i]))
+            except ValueError:  # catches non-json formatted lines
+                pass  # should print an error here
+
+        # we should split out the conditionals to create each line one at a time so that our output is correct
+        key_lines = [l for l in json_lines if 'name' in l and 'prop' in l and 'age' in l['prop']]  # keys exist
+        lines = [l for l in key_lines if l.get('name') and l.get('prop').get('age')]  # keys aren't null
         names = [l['name'] for l in lines]
         ages = [l['prop']['age'] for l in lines]
-        for n,a in zip(names, ages):
-            output.append(n + '\t' + str(a))
-        
-os.system('cd ../..')
-os.system('pwd')
-path = '/srv/runme/'
-prefix = sys.argv[1]
+        for n, a in zip(names, ages):
+            outlist.append(n + '\t' + str(a))
 
-# tmp = path + 'tmp/'
-# if not os.path.exists(tmp):
-#     os.mkdir(tmp)
-# os.system('mv ' + path + prefix + '.txt ' + path + prefix + '.json')
-# os.system('rm ' + path + prefix + '.txt')
-# files = [filename for filename in os.listdir('.') if filename.startswith(prefix)]
 
-files = glob.glob(path + prefix + '*')
-print "files:" , files
-output = []
-for f in files:
-    try:
-        extract_Data(f, output)
-    except ValueError:
-        print "BAD FILE oops."
+if __name__ == "__main__":
+    os.system('cd ../..')
+    os.system('pwd')
+    path = '/srv/runme/'
+    prefix = sys.argv[1]
 
-# move the prefix.txt out of temp
-# os.system('mv ' + path + 'tmp/' + prefix + '.txt ' + path + prefix + '.txt')
-# os.system('mv ' + 'tmp/' + prefix + '.txt ..')
-# os.system('rm -rf ' + tmp)
+    # os.system('mv ' + path + prefix + '.txt ' + path + prefix + '.json')
+    # os.system('rm ' + path + prefix + '.txt')
+    # files = [filename for filename in os.listdir('.') if filename.startswith(prefix)]
 
-outfile = path + '{}.txt'.format(prefix)
-os.system('rm  ' + outfile)
+    files = glob.glob(path + prefix + '*')
+    print "files:" , files
+    output = []
+    for f in files:
+        try:
+            extract_Data(f, output)
+        except ValueError:
+            print "BAD FILE oops."
 
-with open(outfile, 'w') as f:
-    f.write('\n'.join(output)+'\n')
+    outfile = path + '{}.txt'.format(prefix)
+    os.system('rm  ' + outfile)
+
+    with open(outfile, 'w') as f:
+        f.write('\n'.join(output)+'\n')
