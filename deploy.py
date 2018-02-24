b1bb040de367bd8a2130b2404b916bf50c34ca07
Kaya Tollas <kaya@Kayas-MacBook-Pro.local>
2018-02-24 02:36:46 -0800

    this is getting annoying

--- deploy.py
+++ deploy.py
@@ -47,11 +47,11 @@ def deploy(path_to_ssh_key_private_key, server_address, prefix):
 
     ssh = connect_and_pull(path_to_ssh_key_private_key, server_address)
     #write_cron(ssh, prefix)
-    ssh.exec_command('export FLASK_APP=serverlogging.py')
+    # ssh.exec_command('export FLASK_APP=serverlogging.py')
     # ssh.exec_command('flask run')
     print "Launching server at " + server_address
-    ssh.exec_command('gunicorn -D --threads 4 -b 0.0.0.0:8080 --access-logfile \
-                         server.log --timeout 360 serverlogging:app {}'.format(prefix))
+    ssh.exec_command('gunicorn -D --threads 4 -b {}:8080 --access-logfile \
+                         server.log --timeout 360 serverlogging:app {}'.format(server_address,prefix))
 
     # ssh.exec_command('python sprintSquad/procData.py ' + prefix)
     ssh.close()
