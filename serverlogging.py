c4fc22a2c3506b2c3596f5e64de60bedaac95d31
John Rumpel <johnrumpel@Johns-MacBook-Pro.local>
2018-02-23 17:25:04 -0800

    please

---
+++ serverlogging.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import logging, logging.handlers
+
+app = Flask(__name__)
+
+LOG_PATH = 'logs/Raw.txt'
+
+@app.route('/', methods=['POST'])
+def readWriteJSON():
+	del app.logger.handlers[:]
+	app.logger.addHandler(logging.handlers.TimedRotatingFileHandler(LOG_PATH, when='m', interval = 2))
+	app.logger.setLevel(logging.INFO)
+	result = request.get_json()
+	app.logger.info(repr(result))
+	####CHECK VALID JSON
+	valid_json = True
+	if valid_json:
+		with open("proc.txt","a") as fo:
+			 fo.write(repr(result))
+	return repr(result)
+
+app.run(host= '0.0.0.0', port = 8080)
