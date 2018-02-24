72725706c06cd3da8aade19e5eaafea1de6dede3
Kaya Tollas <kaya@Kayas-MacBook-Pro.local>
2018-02-24 11:56:43 -0800

    this is getting annoying

--- serverlogging.py
+++ serverlogging.py
@@ -10,8 +10,8 @@ from procData2 import write_JSON_if_valid
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-# def readWriteJSON(prefix):
-def readWriteJSON():
+def readWriteJSON(prefix):
+# def readWriteJSON():
 	prefix = 'prefix'
 	outpath = '/srv/runme/' + prefix
 	os.chdir(os.path.expanduser(os.getcwd())) #move to home directory
@@ -30,7 +30,7 @@ def readWriteJSON():
 	return repr(json_blob)
 
 # initialization
-# i = sys.argv.index('serverlogging:app')
-# prefix = sys.argv[i+1]
+i = sys.argv.index('serverlogging:app')
+prefix = sys.argv[i+1]
 
 # app.run(host= '0.0.0.0', port = 8080)
