8a26877f75e4ede8c476388c31b540ff2790d280
Kaya Tollas <kaya@Kayas-MacBook-Pro.local>
2018-02-24 02:18:40 -0800

    this is getting annoying

--- serverlogging.py
+++ serverlogging.py
@@ -1,6 +1,6 @@
 # Launch with
 #
-# gunicorn -D --threads 4 -b 0.0.0.0:5000 --access-logfile serverlogging.log --timeout 60 serverlogging:app prefix
+# gunicorn -D --threads 4 -b 0.0.0.0:8080 --access-logfile server.log --timeout 60 serverlogging:app prefix
 from flask import Flask, request
 import logging, logging.handlers
 from procData2 import write_JSON_if_valid
@@ -9,16 +9,18 @@ from procData2 import write_JSON_if_valid
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-def readWriteJSON(prefix):
+# def readWriteJSON(prefix):
+def readWriteJSON():
+	prefix = 'prefix'
 	log_path = '/srv/runme/{}/Raw.txt'.format(prefix)
 	my_logger = logging.getLogger('MyLogger')
 	my_logger.setLevel(logging.DEBUG)
 	handler = logging.handlers.TimedRotatingFileHandler(log_path, when='m', interval = 2)
 	my_logger.addHandler(handler)
 	json_blob = request.get_json()
-	my_logger.info(repr(result))
+	my_logger.info(repr(json_blob))
 	####CHECK VALID JSON
 	write_JSON_if_valid(json_blob, '/srv/runme/{}/proc.txt'.format(prefix))
 	return repr(result)
 
-# app.run(host= '0.0.0.0', port = 8080)
+app.run(host= '0.0.0.0', port = 8080)
